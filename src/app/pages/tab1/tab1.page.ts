48ae5b9e571dc611950816de0e6fcd64c13c2258
Sonia Castro <soniacastromartel@gmail.com>
2022-04-04 20:56:45 +0100

    0.4 creating reusable component Article

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { NewsRespone } from 'src/app/interfaces';
+import { Article, NewsRespone } from 'src/app/interfaces';
 import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
+  public articles: Article[] = [];
 
   constructor(private newsService: NewsService) {}
 
-  ngOnInit(){
-    this.newsService.getTopHeadLines().subscribe((articles) =>{
-      console.log(articles);
-    });
+  ngOnInit() {
+    this.newsService
+      .getTopHeadLines()
+      .subscribe((articles) => this.articles.push(...articles));
   }
-
 }
